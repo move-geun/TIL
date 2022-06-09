24463748a2a624ee2ddb9e0be047851995a56347
이동근 <movegeun11@gmail.com>
2022-06-10 00:45:10 +0900

    Feat Stackover webscrapper

--- 0606 WebScrapper/stackover.py
+++ 0606 WebScrapper/stackover.py
@@ -14,14 +14,33 @@ def stackover_last_pn():
     return int(max(nums))
 
 
+def company_info(html):
+    title = html.find("a",{"class":"s-link"}).string
+    region_rows = html.find_all("div",{"class":"fs-body1"})
+    region = region_rows[1].get_text()
+    location = region_rows[2].get_text()
+    ex_link = html.find("a",{"class":"s-link"})["href"]
+    link = f'https://stackoverflow.com/{ex_link}'
+    return {
+        "title" : title,
+        "region" : region,
+        "location" : location,
+        "link" : link,
+    }
+
+
 def stackover_page_change(num):
+    view_companys = list()
     for page in range(1, num+1):
         stackover_result = requests.get(f'{URL}&pg={page}')
         stackover_soup = BeautifulSoup(stackover_result.text, 'html.parser')
         companys = stackover_soup.find_all('div',{"class":"-company"})
         for company in companys:
-            print(company.find("a",{"class":"s-link"}).string)
+            view_company = company_info(company)
+            view_companys.append(view_company)
+    return view_companys
 
 
-last_num = stackover_last_pn()
-stackover_page_change(last_num)
+def stackover_get_job():
+    last_num = stackover_last_pn()
+    return stackover_page_change(last_num)
--- 0606 WebScrapper/webscrapper.py
+++ 0606 WebScrapper/webscrapper.py
@@ -1,4 +1,7 @@
 from indeed import indeed_get_job
+from stackover import stackover_get_job
 
-# indeed_get_job()
-
+indeed_jobs = indeed_get_job()
+stackover_jobs = stackover_get_job()
+jobs = indeed_jobs + stackover_jobs
+print(jobs)
