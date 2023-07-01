e528382f1de081c81af223d4f737c570bec1db49
이동근 <movegeun11@gmail.com>
2023-07-02 03:15:34 +0900

    TS로 간단 블록체인 구현하기

--- TS/src/index.ts
+++ TS/src/index.ts
@@ -21,3 +21,36 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockChain = new BlockChain();
+
+blockChain.addBlock("first one");
+blockChain.addBlock("second one");
+blockChain.addBlock("third one");
+
+console.log(blockChain.getBlocks());
